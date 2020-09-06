a57b72ccab6906ed602268735ab9077951e842fe
Bhargodev Arya <bhargodevarya@gmail.com>
2020-09-06 21:21:52 +0530

    java project to process req from API gateway

---
+++ lambda/capture_message/java/project/src/main/java/com/lambda/CaptureContact.java
@@ -0,0 +1,32 @@
+package com.lambda;
+
+import com.amazonaws.services.lambda.runtime.Context;
+import com.amazonaws.services.lambda.runtime.LambdaLogger;
+import com.amazonaws.services.lambda.runtime.LambdaRuntime;
+import com.amazonaws.services.lambda.runtime.RequestHandler;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent;
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3Client;
+import com.amazonaws.services.s3.model.PutObjectResult;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class CaptureContact implements RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+
+    LambdaLogger logger = LambdaRuntime.getLogger();
+
+    @Override
+    public APIGatewayProxyResponseEvent handleRequest(APIGatewayProxyRequestEvent input, Context context) {
+        logger.log("Received request ".concat(input.toString()));
+        APIGatewayProxyResponseEvent apiGatewayProxyResponseEvent = new APIGatewayProxyResponseEvent();
+        String requestBody = input.getBody();
+        AmazonS3 amazonS3Client = AmazonS3Client.builder().withRegion("us-east-1").build();
+        String time = DateTimeFormatter.ISO_LOCAL_DATE_TIME.format(LocalDateTime.now());
+        PutObjectResult putObjectResult = amazonS3Client.putObject("dc-contact-bucket",
+                "contact_".concat(time).concat(".json"), requestBody);
+        apiGatewayProxyResponseEvent.setBody("Created file");
+        return apiGatewayProxyResponseEvent;
+    }
+}
